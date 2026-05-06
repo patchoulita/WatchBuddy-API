2031a7d63e44d83bacfe05dfdb8f78f8a0e064f6
patchoulita <gooddoctahgonzo@gmail.com>
2026-05-06 02:48:11 -0500

    Create manifest.js

---
+++ src/providers/googleDrive/manifest.js
@@ -0,0 +1,14 @@
+// src/providers/googleDrive/manifest.js
+const googleDriveManifest = {
+  id: "googleDrive",
+  provider_name: "Nobody TV",
+  description: "Streaming nowhere...",
+  capabilities: {
+    mediaCatalog: true,
+    streaming: true,
+    search: false,
+    itemDetails: false
+  }
+};
+
+module.exports = googleDriveManifest;
