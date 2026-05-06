cc892cf5fe8bec411591c6e590aa06befab71303
patchoulita <gooddoctahgonzo@gmail.com>
2026-05-06 02:50:58 -0500

    Update index.js

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,6 +1,7 @@
 // src/providers/index.js
 const createGoogleDriveService = require("./googleDrive/service");
 const createGoogleDrivePlugin = require("./googleDrive/plugin");
+const googleDriveManifest = require("./googleDrive/manifest");
 
 const { getGoogleDriveProviderSchema } = require("./googleDrive/metadata");
 
@@ -20,9 +21,10 @@ function createProviders({
     googleDriveService,
     isVideoMimeType
   });
-
+  
   return {
     googleDrive: {
+      manifest: googleDriveManifest,
       service: googleDriveService,
       plugin: googleDrivePlugin,
       getSchema: getGoogleDriveProviderSchema
